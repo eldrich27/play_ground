40f8ad8b9af4833fe0d13838d2160f0f5b4fea41
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-10 11:05:41 +0530

    Adding HTML for step component

--- step-component/src/App.tsx
+++ step-component/src/App.tsx
@@ -1,8 +1,18 @@
+import "./index.css"
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Step Component</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div className="circle">1</div>
+        <div className="circle">2</div>
+        <div className="circle">3</div>
+      </div>
+      <p className="message">Step 2 : Some message</p>
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
     </div>
   )
 }
