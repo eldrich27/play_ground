1217c4120d601772f1ca275723232fb8715fbb27
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-10 11:14:12 +0530

    Added logic to make higlight steps and send messages dynamically

--- step-component/src/App.tsx
+++ step-component/src/App.tsx
@@ -1,17 +1,26 @@
 import "./index.css"
 
+const messages:string[] = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step:number = 3;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="circle">1</div>
-        <div className="circle">2</div>
-        <div className="circle">3</div>
+        <div className={`${step >= 1 && 'active'}`}>1</div>
+        <div className={`${step >= 2 && 'active'}`}>2</div>
+        <div className={`${step >= 3 && 'active'}`}>3</div>
+        
       </div>
-      <p className="message">Step 2 : Some message</p>
+      <p className="message">Step {step} : {messages[step - 1]}</p>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button className="previous">Previous</button>
+        <button className="next">Next</button>
       </div>
     </div>
   )
