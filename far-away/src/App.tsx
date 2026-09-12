47dd9052862ca0d2bb5c9e7d7aec03100f825eb1
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-12 18:12:51 +0530

    Added logo and removed legacy index.css

--- far-away/src/App.tsx
+++ far-away/src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
+import {Logo} from './components/Logo'
 
 import './App.css'
 
 function App() {
  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Logo />
   )
     
 }
---
+++ far-away/src/components/Logo.tsx
@@ -0,0 +1,9 @@
+
+
+export function Logo() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      🏝️Far Away🎒
+    </h1>
+  )
+}
