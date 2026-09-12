fdf20dad1eb239a1cdbbae61c9f1e7f288ba3b11
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-12 18:37:24 +0530

    Added for component

--- far-away/src/App.tsx
+++ far-away/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import {Logo} from './components/Logo'
+
+import {Logo} from './components/Logo';
+import {Form} from './components/Form';
 
 import './App.css'
 
 function App() {
  return (
+  <>
     <Logo />
+    <Form />
+  </>
   )
     
 }
---
+++ far-away/src/components/Form.tsx
@@ -0,0 +1,13 @@
+
+
+export function Form() {
+  return (
+    <form className="add-form">
+      <h3> 
+        What do you need😍 for your trip?
+      </h3>
+      <input type="text" id="name" name="name" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
