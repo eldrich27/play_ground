d7f80b7c516f6a3d8009b6465fec29f1cbcc96e5
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-12 12:30:32 +0530

    Adding the close button

--- step-component/src/App.tsx
+++ step-component/src/App.tsx
@@ -26,22 +26,25 @@ export default function App() {
 
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 && 'active'}`}>1</div>
-        <div className={`${step >= 2 && 'active'}`}>2</div>
-        <div className={`${step >= 3 && 'active'}`}>3</div>
-        
+    <>
+      <button className="close" onClick={() => setStep(1)}>&times;</button>
+      <div className="steps">
+        <div className="numbers">
+          <div className={`${step >= 1 && 'active'}`}>1</div>
+          <div className={`${step >= 2 && 'active'}`}>2</div>
+          <div className={`${step >= 3 && 'active'}`}>3</div>
+          
+        </div>
+        <p className="message">Step {step} : {messages[step - 1]}</p>
+        <div className="buttons">
+          <button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={handlePrev}>
+            Previous
+          </button>
+          <button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={handleNext}>
+            Next
+          </button>
+        </div>
       </div>
-      <p className="message">Step {step} : {messages[step - 1]}</p>
-      <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={handlePrev}>
-          Previous
-        </button>
-        <button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+    </>
   )
 }
