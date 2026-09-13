4268c1b16664f69cfd1eb00963e8be8b066f2d08
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-13 19:38:27 +0530

    Passing Items to the list component

--- far-away/src/components/List.tsx
+++ far-away/src/components/List.tsx
@@ -1,14 +1,15 @@
 import {ListItem} from './ListItems';
 import type {ComponentProps} from 'react';
+import type {Items} from '../types'
 
 type ListProps = ComponentProps<typeof ListItem>;
 
-export function List({items = []}: {items?: ListProps['item'][]}) {
+export function List({items = []}: {items?: Items[]}) {
     return (
         <div className="list">
             <ul>
-                {items.map((item, index) => (
-                    <ListItem key={index} number={index + 1} item={item} />
+                {items.map((item) => (
+                    <ListItem key={item.id} number={item.quantity} item={item.item} />
                 ))}
             </ul>
         </div>
