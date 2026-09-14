941008cc3f598bd24331a53aedd1f40960058deb
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-14 10:14:45 +0530

    Sort functionality added

--- far-away/src/components/List.tsx
+++ far-away/src/components/List.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {ListItem} from './ListItems';
 // import type {ComponentProps} from 'react';
 import type {Items} from '../types'
@@ -6,7 +7,21 @@ import type {Items} from '../types'
 
 export function List({items = [], onRemoveItem, onTogglePacked}: {items?: Items[]; onRemoveItem: (id: number) => void; onTogglePacked: (id: number) => void}) {
     
-    const sortedItems = [...items].sort((a, b) => a.item.localeCompare(b.item));
+    // use state to manage the sorting and filtering options
+    const [sortOption, setSortOption] = useState('input');
+    let sortedItems:Items[] = [];
+
+    if (sortOption === 'input') {
+        sortedItems = items;
+
+    } else if (sortOption === 'Item') {
+        // sort by item name
+        sortedItems = [...items].sort((a, b) => a.item.localeCompare(b.item));
+    } else if (sortOption === 'packed') {
+        // sort by packed status
+        sortedItems = [...items].sort((a, b) => Number(!a.packed) - Number(!b.packed));
+    }
+   
     
     return (
         <div className="list">
@@ -15,7 +30,7 @@ export function List({items = [], onRemoveItem, onTogglePacked}: {items?: Items[
                     <ListItem key={item.id} item={item} onRemoveItem={onRemoveItem} onTogglePacked={onTogglePacked} />
                 ))}
             </ul>
-            <div className="actions">
+            <div className="actions" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
                 <select className="select">
                     <option value="input">Sort by Input Order</option>
                     <option value="Item">Sort by Item Name</option>
