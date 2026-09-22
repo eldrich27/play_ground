c7c56b8fc0da965c15658800297c9d7d30361b1b
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-22 19:54:39 +0530

    Added thereducer hook to the app component

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,17 +1,30 @@
+import { useReducer } from "react"
+
 // import DateCounter from "./components/DateCounter"
 import Main from "./components/Main"
 import Header from "./components/Header"
 import Loader from "./components/Loader"
 import Progress from "./components/Progress"
+import StartScreen from "./components/StartQuiz"
+
+const initialState = {numQuestion:0, maxPoints:10}
+
+function reducer(){
+  return null
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div className="app">
       <Header />
       <Main>
         <Progress />
+        <StartScreen />
       </Main>
+
     </div>
   )
 }
