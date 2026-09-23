ef30824bc8d07ccbe4c4df119f9611fafeecd5b2
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-23 11:48:11 +0530

    updated the styling for the buttons and displayed the options in the questions componnet

--- react-quiz/src/components/Questions.tsx
+++ react-quiz/src/components/Questions.tsx
@@ -9,6 +9,12 @@ function Question({ question }: QuestionsProps) {
   return (
     <div>
       <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((option) => (
+            <button className="btn btn-option" key={option}>{option}</button>
+        ))}
+        
+      </div>
     </div>
   );
 }
