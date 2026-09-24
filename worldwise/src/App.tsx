040de12d4eaced370163a2c969da3ad168a88ce4
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-24 11:11:24 +0530

    Aded pagenot found and implemented routes for product pricing and home page in the app

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -1,10 +1,20 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { Home } from './pages/Home'
+import { Products } from './pages/Products'
+import { Pricing } from './pages/Pricing'
 
 function App() {
 
   return (
-    <h1>Worldwise!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ worldwise/src/pages/PageNotFound.tsx
@@ -0,0 +1,7 @@
+
+
+export function Products(){
+    return(
+        <h1>404 : Page Not found</h1>
+    )
+}
