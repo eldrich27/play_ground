585be19d09ce78bbafa9cdf30157f41dc072df28
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-25 10:28:55 +0530

    Added teh homepage to te UI

--- worldwise/src/App.tsx
+++ worldwise/src/App.tsx
@@ -1,17 +1,17 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Home } from './pages/Home'
+import Homepage from './pages/Homepage'
 import { Products } from './pages/Products'
-import { Pricing } from './pages/Pricing'
-import { PageNotFound } from './pages/PageNotFound'
+import { Pricing } from './pages/Pricing.tsx'
+import { PageNotFound } from './pages/PageNotFound.tsx'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={ <Homepage />} />
         <Route path='/products' element={<Products/>}/>
         <Route path='/pricing' element={<Pricing/>}/>
         <Route path='/*' element={<PageNotFound/>}/>
