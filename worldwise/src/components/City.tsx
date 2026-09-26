f4046279bb453a21355a7e01c3b5fd5fc59aef73
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-26 17:33:46 +0530

    Setting lat and lng in the city component

--- worldwise/src/components/City.tsx
+++ worldwise/src/components/City.tsx
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom"
+import { useParams, useSearchParams } from "react-router-dom"
 
 
 export function City(){
     const { id } = useParams<{ id: string }>()
+
+    const [searchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
     return(
         <div>
             <h2>City {id}</h2>
+            {lat && <p>lat : {lat}</p>}
+            {lng && <p>lng : {lng}</p>}
         </div>
     )
 }
