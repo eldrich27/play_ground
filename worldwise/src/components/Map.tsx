dc28af43b80997100e66aebfd888021eeba98af8
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-26 16:41:24 +0530

    Added map container style to map

--- worldwise/src/components/Map.tsx
+++ worldwise/src/components/Map.tsx
@@ -1,9 +1,9 @@
-
+import style from "./Map.module.css"
 
 
 export default function Map(){
     return(
-        <div className="map">
+        <div className={style.mapContainer}>
             <h1>Welcome from Maps!</h1>
         </div>
     )
--- worldwise/src/pages/AppLayout.tsx
+++ worldwise/src/pages/AppLayout.tsx
@@ -1,11 +1,13 @@
 import styles from "./AppLayout.module.css";
 import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 
 
 export default function AppLayout(){
     return (
         <main className={styles.app}>
             <Sidebar />
+            <Map />
         </main>
     )
 }
