f16b652f9a1f28edb30db05a6281701381bf2a46
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-26 09:50:13 +0530

    Included teh sidebar in the apps component

--- worldwise/src/pages/AppLayout.tsx
+++ worldwise/src/pages/AppLayout.tsx
@@ -1,18 +1,11 @@
 import styles from "./AppLayout.module.css";
+import Sidebar from "../components/Sidebar";
 
 
 export default function AppLayout(){
     return (
         <main className={styles.app}>
-      <section>
-        <h1>
-          You travel the world.
-          <br />
-          WorldWise keeps track of your adventures.
-          <br />
-          Hellow from the App page
-        </h1>
-      </section>
-    </main>
+            <Sidebar />
+        </main>
     )
 }
