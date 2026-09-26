b7f57c98cc0fd06ec16585ab6f11bb7471395618
Eldrich Pereira <pereiraeldrich27@gmail.com>
2026-09-26 12:22:24 +0530

    Added teh delete button in the citylist

--- worldwise/src/components/CityItem.tsx
+++ worldwise/src/components/CityItem.tsx
@@ -31,6 +31,7 @@ export function CityItem({city}:CityItemProps){
         <span className={style.emoji}>{flagEmojiToPNG(city.emoji)}</span>
         <p className={style.name}>{city.cityName}</p>
         <time className={style.date} dateTime={city.date}>{formatDate(city.date)}</time>
+        <button className={style.deleteBtn}>&times;</button>
     </li>
  )
 }
